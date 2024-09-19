d21f476926888fa3eac95472da26066b8de2255c
Ubuntu <ubuntu@ip-172-31-33-121.ap-southeast-2.compute.internal>
2024-09-19 06:25:40 +0000

    added snippet for output

--- myfirstfile.tf
+++ myfirstfile.tf
@@ -1,15 +1,36 @@
-provider "aws" {
-  region     = "ap-southeast-1"
-  access_key = "AKIAXVTO4X6AP2EYXQN3"
-  secret_key = "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"
-}
 
 resource "aws_instance" "web" {
-  count = 3
+#  count = 3
   ami           = "ami-05c880175794030cd"
   instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+output "ec2myname" {
+  value = aws_instance.web.public_ip
+}
+
+resource "aws_s3_bucket" "s3_bucket_example" {
+  bucket = "39893843-my-tf-test-bucket-1"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+output "s3bucketName" {
+  value = aws_s3_bucket.s3_bucket_example.id
+}
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.web.id
+  domain   = "vpc"
+}
+
+output "aws-eip" {
+  value = aws_eip.lb.public_ip
 }
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "ap-southeast-1"
+  access_key = "AKIAXVTO4X6AP2EYXQN3"
+  secret_key = "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"
+}
