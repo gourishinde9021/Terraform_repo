fee9f8123b7c3784273416660e40e09da4ee46c5
gourishinde9021 <ubuntu@ip-172-31-33-121.ap-southeast-2.compute.internal>
2024-09-19 08:37:24 +0000

    added assignment 2 to create vpc and sg

---
+++ assignment_2/create_sg.tf
@@ -0,0 +1,31 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_443" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_80" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+
